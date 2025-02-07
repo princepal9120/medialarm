ef0c19ca2f2f15de5464e4c00557b9a0109a9b84
Prince Pal <princepal9120@gmail.com>
2025-02-07 12:00:20 +0530

    create a splash screen of app

---
+++ app/index.tsx
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { useState, useRef, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function SplashScreen() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  return (
+    <View style={style.container}>
+      <Animated.View
+        style={[
+          style.iconContainer,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Ionicons name="medical" size={100} color="white"></Ionicons>
+        <Text style={style.appName}>MediAlert</Text>
+      </Animated.View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#4CAF50",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    alignItems: "center",
+  },
+  appName: {
+    color: "white",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20,
+    letterSpacing: 1,
+  },
+});
